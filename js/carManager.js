34bcc60804cc3f710352ab1f8e59dfbc19e8c9b0
jeffa00 <jeffa00@gmail.com>
2017-01-25 13:58:22 -0500

    Added ability to edit vehicles.

--- js/carManager.js
+++ js/carManager.js
@@ -1,11 +1,14 @@
 (function(){
     var saveButton = document.getElementById("save-button"),
+        clearButton = document.getElementById("clear-button"),
+        currentVehicleId = document.getElementById("vehicle-id"),
         carYear = document.getElementById("car-year"),
         carMake = document.getElementById("car-make"),
         carModel = document.getElementById("car-model"),
         carDoors = document.getElementById("car-doors"),
         vehicleList = document.getElementById("vehicle-list"),
-        maxVehicleId = 0;
+        maxVehicleId = 0,
+        vehicles = [];
 
     var Automobile = function Automobile(year, make, model){
         this.id = maxVehicleId++;
@@ -34,27 +37,59 @@
 
     Car.prototype = Object.create(Automobile.prototype);
 
+    var editClickHandler = function editClickHandler(e){
+        var currentVehicle = vehicles[e.currentTarget.id];
+
+        currentVehicleId.value = currentVehicle.id;
+        carYear.value = currentVehicle.year;
+        carMake.value = currentVehicle.make;
+        carModel.value = currentVehicle.model;
+        carDoors.value = currentVehicle.doors;
+    }
+
+    var renderList = function renderList(){
+        vehicleList.innerHTML = "";
+
+        for(vehidx in vehicles){
+            var curVehicle = vehicles[vehidx];
+
+            var newItem = document.createElement("li");
+            newItem.classList.add("list-group-item");
+
+            var newAnchor = document.createElement("a");
+            newAnchor.id = curVehicle.id;
+            newAnchor.href ="#";
+            newAnchor.innerText = curVehicle.getFullName();
+            newAnchor.addEventListener("click", editClickHandler);
+
+            newItem.appendChild(newAnchor);
+
+            vehicleList.appendChild(newItem);
+        }
+    }
+
+    var clearFields = function clearFields(){
+        currentVehicleId.value = "";
+        carYear.value = "2017";
+        carMake.value = "";
+        carModel.value = "";
+        carDoors.value = "4";
+    }
+
+    clearButton.addEventListener("click", clearFields);
+
     saveButton.addEventListener("click", function(){
-        // var car = {
-        //     Year: carYear.value,
-        //     Make: carMake.value,
-        //     Model: carModel.value,
-        //     Doors: carDoors.value,
-        //     getFullName: function(){
-        //         return this.Year + " " +
-        //                 this.Make + " " +
-        //                 this.Model + " " +
-        //                 this.Doors;
-        //     }
-        // };
+        var newCar = vehicles[currentVehicleId.value] || new Car(carYear.value, carMake.value, carModel.value, carDoors.value);
 
-        var newCar = new Car(carYear.value, carMake.value, carModel.value, carDoors.value);
+        newCar.year = carYear.value;
+        newCar.make = carMake.value;
+        newCar.model = carModel.value;
+        newCar.doors = carDoors.value;
 
-        var newItem = document.createElement("li");
-        newItem.innerText = newCar.getFullName();
-        newItem.classList.add("list-group-item");
+        vehicles[newCar.id] = newCar;
 
-        vehicleList.appendChild(newItem);
+        renderList();
+        clearFields();
     });
 
 })();
